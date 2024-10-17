e634b598f536364b706f5a34a3da8267ed55f1c7
okarpenko <okarpenko@usfca.edu>
2024-10-17 08:44:05 -0700

    Added a blocking queue exercise.

---
+++ src/main/java/blockingqueue/BlockingQueue.java
@@ -0,0 +1,42 @@
+package blockingqueue;
+
+import java.util.LinkedList;
+
+/** BlockingQueue exercise/example - demonstrates the use of wait() and notify().
+ * From http://tutorials.jenkov.com/java-concurrency/blocking-queues.html
+ */
+public class BlockingQueue {
+
+	private int capacity;
+	private LinkedList<Integer> queue = new LinkedList<>();
+
+	public BlockingQueue(int capacity) {
+		this.capacity = capacity;
+	}
+
+	/**
+	 * Waits until there is space in the queue, then adds an element to the queue
+	 * @param element the element to be added to the queue
+	 * @throws InterruptedException
+	 */
+	public synchronized void enqueue(Integer element) throws InterruptedException {
+		// FILL IN CODE:
+		// if the queue is full, you need to wait
+		// otherwise, add an element to the queue and notify everyone who is waiting on the queue
+
+	}
+
+	/**
+	 * Waits until the queue has something to dequeue, then dequeues it and returns the element.
+	 * @return the first element from the queue
+	 * @throws InterruptedException
+	 */
+	public synchronized Integer dequeue() throws InterruptedException {
+		Integer elem  = null;
+		// If the queue is empty, need to wait
+		// When the queue is not empty, we can dequeue an element and
+		// notify everyone who is waiting on this queue
+
+		return elem;
+	}
+}
