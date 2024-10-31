43bedbbb2ca6c12b98fddc733e03dd5fecbe5693
okarpenko <okarpenko@usfca.edu>
2024-10-31 09:41:00 -0700

    Added an exercise on servlets.

---
+++ src/main/java/servlets/movieServlet/MovieServer.java
@@ -0,0 +1,31 @@
+package servlets.movieServlet;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieServer {
+    public static final int PORT = 6500;
+
+    public static void main(String[] args) {
+        Server server = new Server(PORT);
+        ServletHandler handler = new ServletHandler();
+
+        // when the user goes to http://localhost:6500/movies, the get request is
+        // going to go to MovieServlet
+        handler.addServletWithMapping(new ServletHolder(new MovieServlet()), "/movies");
+
+        server.setHandler(handler);
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            System.out.println("Could not start the server");
+        }
+    }
+}
---
+++ src/main/java/servlets/movieServlet/MovieServlet.java
@@ -0,0 +1,40 @@
+package servlets.movieServlet;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.commons.text.StringEscapeUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieServlet extends HttpServlet {
+    private List<Movie> movies = new ArrayList<>();
+
+    public MovieServlet() {
+        Movie m1 = new Movie("Inception", "Christopher Nolan", 2010);
+        Movie m2 = new Movie("Titanic", "James Cameron", 1997);
+        Movie m3 = new Movie("The Godfather", "Francis Ford Coppola", 1972);
+        movies.add(m1);
+        movies.add(m2);
+        movies.add(m3);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = response.getWriter();
+        // FILL IN CODE to create a response (html with movies
+
+
+    }
+
+}
